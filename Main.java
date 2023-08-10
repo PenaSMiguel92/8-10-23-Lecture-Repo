e1dd1d1269f675e91307e4ed9da33aa3cf4eac13
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-10 14:22:06 -0700

    additional notes

--- Main.java
+++ Main.java
@@ -7,6 +7,30 @@
  * 
  * The heap is where garbage collection happens. Note: we cannot force garbage collection in Java.
  * We can only suggest garbage collection to happen using System.gc() but it is not guaranteed to occur.
+ * 
+ * So how does Java handle garbage collection?
+ * 
+ * When memory runs low, Java runs the garbage collector to remove what it isn't needed so we can 
+ * reuse the space in memory.
+ * 
+ * 1) reference goes out of scope.
+ * public void example() {
+ * Cat c = new Cat();
+ * }
+ * reference to c goes bye bye, which makes the object eligible for garbage collection.
+ * 
+ * 2) The reference is assigned to another object
+ * 
+ * Cat c = new Cat();
+ * c = new Cat();
+ * 
+ * My first object is left in the dust when it was "reprogrammed" to a new object.
+ * 
+ * 3) The reference is explicitly set to null;
+ * 
+ * Cat c = new Cat();
+ * c = null;
+ * 
  */
 
 public class Main {
