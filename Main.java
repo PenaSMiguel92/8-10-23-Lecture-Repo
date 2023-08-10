7e7325d21eeba6ef5137ae24a6a8a144b319b749
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-10 14:09:27 -0700

    additional notes

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+/*
+ * Garbage Collection - 
+ * Every time an object is created, it goes into heap memory. 
+ * 
+ * Different objects take up different amounts of memory space.
+ * An object with 20 different instance variable will take up more space than an object with 5 different instance variables.
+ * 
+ * The heap is where garbage collection happens. Note: we cannot force garbage collection in Java.
+ * We can only suggest garbage collection to happen using System.gc() but it is not guaranteed to occur.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
